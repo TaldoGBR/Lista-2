19c286ac0f8da7867420df83fbcd254e2d30fbd4
TaldoGBR <165737345+TaldoGBR@users.noreply.github.com>
2024-05-13 21:40:14 -0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1;
+            double p2;
+
+            p1 = float(input("Digite a nota da primeira avaliação (P1): "));
+
+            p2 = (15 - p1) / 2;
+
+            Console.Write(f"Para ser aprovado com média 5, você precisa tirar {p2_necessaria:.2f} na segunda avaliação (P2).");
+
+        }
+    }
+}
